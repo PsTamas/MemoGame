951693681a8eea989966745c2acce33d091a330c
Póra-Simon Tamás <poratamas01@gmail.com>
2017-07-12 12:52:17 +0200

    Models,ViewModels setup.

---
+++ MemoGame/Models/CardModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MemoGame.Models
+{
+    class CardModel
+    {
+        public enum Size { Small, Medium, Large };
+
+        public Color CardColor { get; private set; }
+        public Size CardSize { get; private set; }
+        public BitmapImage CardImage { get; private set; }
+
+        public CardModel(Size cardSize, Color cardColor, BitmapImage cardImage)
+        {
+            this.CardColor = cardColor;
+            this.CardSize = cardSize;
+            this.CardImage = cardImage;
+        }
+    }
+
+  
+
+
+}
+
---
+++ MemoGame/ViewModels/CardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoGame.ViewModels
+{
+    class CardViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
